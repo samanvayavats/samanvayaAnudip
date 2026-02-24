36235f78bba52836d62c95cfa3a0296ab974b891
samanvaya <samayvats@gmail.com>
2026-02-24 10:14:09 +0530

    first problem swapping using the three var

---
+++ 100Problems/swapUsingThreeVar.py
@@ -0,0 +1,15 @@
+# Problem 1
+# Problem Statement: Swap two numbers without using a third variable.
+# Input Format: Read input from standard input.
+# Output Format: Print the required output.
+# Constraints: Standard constraints apply.
+
+a = 5 
+b=6
+print('before swaping the value is : ' , a , b)
+
+temp = a
+a = b 
+b = temp
+
+print('After swaping the value is : ' , a , b)
--- lab_1_23-feb-2026/listrRelation.py
+++ lab_1_23-feb-2026/listrRelation.py
@@ -1,21 +1,40 @@
 # to create a list 20 numbers and ask the user to input any number and delete this number from the list from its all the occurences except the 
 # first occurence 
 
-numberList = [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,]
-count = True
+numberList = [1,1,1,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]
+# count = True
 userNumber =int(input('enter the number which u want to remove from the list '))
-numberList.reverse()
+# numberList.reverse()
+
+# if userNumber in numberList:
+#     for i in numberList:
+#         if(i == userNumber and count==True):
+#             count=False
+#         else:
+#             if(i==userNumber and count==False ):
+#                 numberList.remove(userNumber)
+
+# else:
+#     print('number not exits')
+
+# numberList.reverse()
+
+firstIndex = -1 
 
 if userNumber in numberList:
-    for i in numberList:
-        if(i == userNumber and count==True):
-            count=False
-        else:
-            if(i==userNumber and count==False ):
-                numberList.remove(userNumber)
+    for i in range(0 ,len(numberList)):
+        if(numberList[i]==userNumber):
+            firstIndex = i
+            break
 
 else:
     print('number not exits')
 
-numberList.reverse()
+if firstIndex != -1:
+    for i in range(len(numberList) - 1, firstIndex, -1):
+        if numberList[i] == userNumber:
+            numberList.pop(i)
+
+
+print(firstIndex)
 print(numberList)
