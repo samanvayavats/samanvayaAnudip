1d11c4cbf77540f62026b4968be73ef0c9d4c829
samanvaya <samayvats@gmail.com>
2026-02-24 10:30:26 +0530

    negative and postive poblem

---
+++ 100Problems/posOrNeg.py
@@ -0,0 +1,12 @@
+# Problem 2
+# Problem Statement: Check whether a number is positive, negative, or zero.
+# Input Format: Read input from standard input.
+# Output Format: Print the required output.
+# Constraints: Standard constraints apply.
+
+a = int(input('enter the number '))
+
+if a>0:
+    print('the number is positive')
+else:
+    print('the number is neagative')
